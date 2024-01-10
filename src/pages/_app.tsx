bc97bdfafced472e0a48a09948753d98b71fa318
orl0pl <orlyyt@int.pl>
2024-01-10 21:39:46 +0100

    init2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, AdaptiveProvider, Button } from '@znui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <AdaptiveProvider >
+    <Component {...pageProps} />
+  </AdaptiveProvider >
+)
+
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
+import { ThemeProvider } from '@znui/react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <ThemeProvider scheme='system'>
+        <Html lang="en">
+          <Head />
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+        </ThemeProvider>
   )
 }
